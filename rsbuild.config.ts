72f8e89c5b1b39dea6a1913f98b0b0436d27c81b
AlcMaple <2933724627@qq.com>
2026-08-15 16:13:19 +0800

    fix(mobile): 固定实时调试端口

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5678,
-    strictPort: false,
+    strictPort: true,
     printUrls: false,
     open: false,
     compress: false,
